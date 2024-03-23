f821f743d8624712f6e4e2b5c7e6ea421f685aec
4l3-j4ndr0 <al3jandro9400@gmail.com>
2024-03-23 15:40:47 -0400

    changes: correction of the form

--- assets/js/popup.js
+++ assets/js/popup.js
@@ -1,25 +1,20 @@
+// Tu JavaScript con la lógica para abrir y cerrar el popup
 let btnAbrirPopup = document.getElementById('btn-abrir-popup'),
-	overlay = document.getElementById('overlay'),
-	popup = document.getElementById('popup'),
-	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
-    btnSendMessage = document.getElementById('sendMessage');
+    overlay = document.getElementById('overlay'),
+    popup = document.getElementById('popup'),
+    btnCerrarPopup = document.getElementById('btn-cerrar-popup');
 
-btnAbrirPopup.addEventListener('click', function(){
-    document.body.classList.add('no-scroll'); // Añade la clase para bloquear el desplazamiento
-	overlay.classList.add('active');
-	popup.classList.add('active');
+btnAbrirPopup.addEventListener('click', function() {
+    document.body.classList.add('no-scroll');
+    overlay.classList.add('active');
+    popup.classList.add('active');
 });
 
-btnCerrarPopup.addEventListener('click', function(e){
-	e.preventDefault();
-    document.body.classList.remove('no-scroll'); // Elimina la clase para permitir el desplazamiento
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
+btnCerrarPopup.addEventListener('click', function(e) {
+    e.preventDefault();
+    document.body.classList.remove('no-scroll');
+    overlay.classList.remove('active');
+    popup.classList.remove('active');
 });
 
-btnSendMessage.addEventListener('click', function(e){
-	e.preventDefault();
-    document.body.classList.remove('no-scroll'); // Elimina la clase para permitir el desplazamiento
-	overlay.classList.remove('active');
-	popup.classList.remove('active');
-});
+// No necesitas un manejador de evento para 'sendMessage' si sólo deseas que el formulario se envíe normalmente.
